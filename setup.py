4dd884820b03079d6d782e88e6a18a782a90df54
VaniSuryavanshi <vanisuryavanshi.003@gmail.com>
2024-12-26 13:46:01 +0530

    Fix: Rename src_repo from text-summarizer to textSummarizer in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer"
 AUTHOR_USER_NAME = "VaniSuryavanshi"
-SRC_REPO = "text-summarizer"
+SRC_REPO = "textSummarizer"
 AUTHOR_EMAIL = "vanisuryavanshi.003@gmail.com"
 
 setuptools.setup(
